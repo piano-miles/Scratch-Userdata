3fb8e6666b9bcfb98cb1a7f6302c9e82d03e3105
miles waugh <miles.m.waugh@gmail.com>
2022-10-28 00:08:01 -0700

    Update

--- main.py
+++ main.py
@@ -1,77 +1,102 @@
-import os # Operating Systemß
-import requests # Getting API requests
-import json # Reading JSON data
+import os  # Operating System
+import requests  # Handle API requests
+import json  # Read JSON data
 
 print("----------------")
+
+
+def printl(label, data):
+    print("\n"+label+":")
+    print(data)
+
 
 def jdump(obj):
     # Create a formatted string of the Python JSON object
     return json.dumps(obj, sort_keys=True, indent=4)
 
-try:
-    # Featured
-    response = requests.get("https://api.scratch.mit.edu/proxy/featured")
 
-    # Trending explore
-    "https://api.scratch.mit.edu/explore/projects?q=games&mode=trending&language=en"
-
-except:
+def jget(url, log):
+    try:
+        response = requests.get(url)
+    except:
         print("Failed GET request.")
-else:
-    sc = response.status_code
-    print("Status: " + str(sc))
-    if sc == 200:
-        print("Result returned successfully.")
-        #print(response.json())
-        #print(jdump(response.json()))
-        if True:    
-            for p in response.json()['community_featured_projects']:
-                if p['type'] == "project":
-                    creator = p['creator']
-                    project_id = p['id']
-                    project_title = p['title']
-                    loves = p['love_count']
-                    
-                    # Creator info
-                    #"https://api.scratch.mit.edu/users/"+creator
-
-                    # Favorited projects
-                    #"https://api.scratch.mit.edu/users/"+creator+"/favorites"
-
-                    # Followers
-                    #"https://api.scratch.mit.edu/users/"+creator+"/followers"
-
-                    # Following
-                    #"https://api.scratch.mit.edu/users/"+creator+"/following"
-
-                    # Unread messages
-                    #"https://api.scratch.mit.edu/users/"+creator+"/messages/count"
-
-                    # Projects
-                    #"https://api.scratch.mit.edu/users/"+creator+"/projects"
-
-                        # Project Info
-                        #"https://api.scratch.mit.edu/users/"+creator+"/projects/"+project_id
-
-                        # Project Comments
-                        #"https://api.scratch.mit.edu/users/"+creator+"/projects/"+project_id+"/comments"
-
-                    print(creator)
-                    response = requests.get("https://api.scratch.mit.edu/users/"+creator)
+        quit()
+    else:
+        sc = response.status_code
+        if log:
+            print("Status: " + str(sc))
+        if sc == 200:
+            if log:
+                print("Result returned successfully.")
+            return response.json()
+        elif log:
+            if sc == 301:
+                print("The server is redirecting you to a different endpoint.")
+            elif sc == 400:
+                print("The server thinks you made a bad request. Incorrect credentials.")
+            elif sc == 401:
+                print("The server thinks you’re not authenticated.")
+            elif sc == 403:
+                print(
+                    "The resource you’re trying to access is forbidden. You do not have the right permissions.")
+            elif sc == 404:
+                print("The resource you tried to access wasn’t found on the server.")
+            elif sc == 503:
+                print("The server is not ready to handle the request.")
+            else:
+                print("Unknown error type.")
+            quit()
 
 
-                    quit()
+featured = jget("https://api.scratch.mit.edu/proxy/featured", True)  # Featured
+# explore = jget("https://api.scratch.mit.edu/explore/projects?q=games&mode=trending&language=en", True) #Trending explore
 
-            #print(jdump(pass_times))
-        if sc == 301:
-            print("The server is redirecting you to a different endpoint.")
-        if sc == 400:
-            print("The server thinks you made a bad request. Incorrect credentials.")
-        if sc == 401:
-            print("The server thinks you’re not authenticated.")
-        if sc == 403:
-            print("The resource you’re trying to access is forbidden. You do not have the right permissions.")
-        if sc == 404:
-            print("The resource you tried to access wasn’t found on the server.")
-        if sc == 503:
-            print("The server is not ready to handle the request.")
+# print(response.json())
+# print(jdump(response.json()))
+
+
+for p in featured['community_featured_projects']:
+    if p['type'] == "project":
+        creator = p['creator']
+        project_id = p['id']
+        project_title = p['title']
+        loves = p['love_count']
+
+        printl("creator",       creator)
+        printl("project_id",    project_id)
+        printl("project_title", project_title)
+        printl("loves",         loves)
+
+        # Creator info
+        q = "https://api.scratch.mit.edu/users/"+creator
+        printl("user_info <- GET", q)
+        user_info = jget(q, False)
+        printl("user_info", user_info)
+
+        # Favorited projects
+        # "https://api.scratch.mit.edu/users/"+creator+"/favorites"
+
+        # Followers
+        q = "https://api.scratch.mit.edu/users/"+creator+"/followers"
+        printl("followers <- GET", q)
+        followers = jget(q, False)
+        printl("followers", followers)
+
+        # Following
+        # "https://api.scratch.mit.edu/users/"+creator+"/following"
+
+        # Unread messages
+        # "https://api.scratch.mit.edu/users/"+creator+"/messages/count"
+
+        # Projects
+        # "https://api.scratch.mit.edu/users/"+creator+"/projects"
+
+        # Project Info
+        # "https://api.scratch.mit.edu/users/"+creator+"/projects/"+project_id
+
+        # Project Comments
+        # "https://api.scratch.mit.edu/users/"+creator+"/projects/"+project_id+"/comments"
+
+        quit()
+
+    # print(jdump(pass_times))
